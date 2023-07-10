b4c6981dbd89e63847551d7ce7e275876622584a
karmagen88 <marat.karimov@osome.com>
2023-07-10 11:44:02 +0800

    mirate to flask server

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import json
 import os
 import threading
 import time
 import argparse
 import psutil
-from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+from flask import Flask, request, abort
 
 from processor import DataProcessor
 
@@ -15,13 +14,14 @@ heartbeat_timeout_sec = 20
 
 last_heartbeat_time = time.time()
 
-processor = None
+processor: DataProcessor = None
+
+app = Flask(__name__)
 
 
 def kill(data=None):
     response_data = {'message': 'Server is shutting down...'}
     threading.Timer(1.0, os._exit, args=(0,)).start()
-
     return response_data
 
 
@@ -33,7 +33,8 @@ def get_memory_usage():
     return {'heartbeat': 'pong', 'memory_usage_mb': memory_usage_mb}
 
 
-def heartbeat(data=None):
+@app.route('/heartbeat', methods=['POST'])
+def heartbeat():
     global last_heartbeat_time
     last_heartbeat_time = time.time()
 
@@ -41,7 +42,6 @@ def heartbeat(data=None):
 
 
 def init_engine(engine):
-
     global processor
 
     if engine == 'duckdb':
@@ -51,50 +51,35 @@ def init_engine(engine):
     else:
         raise Exception('Unknown engine')
 
-    endpoint_to_function.update({
-        '/import_file': processor.import_file,
-        '/run_sql': processor.run_sql,
-        '/get_schema': processor.get_schema,
-        '/export_file': processor.export_file,
-        '/validate': processor.validate,
-    })
+
+@app.route('/import_file', methods=['POST'])
+def import_file():
+    return processor.import_file(request.get_json())
 
 
-endpoint_to_function = {
-    '/kill': kill,
-    '/heartbeat': heartbeat,
-}
+@app.route('/run_sql', methods=['POST'])
+def run_sql():
+    return processor.run_sql(request.get_json())
 
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def _set_response(self, status_code=200, content_type='text/plain'):
-        self.send_response(status_code)
-        self.send_header('Content-type', content_type)
-        self.end_headers()
+@app.route('/get_schema', methods=['POST'])
+def get_schema():
+    return processor.get_schema(request.get_json())
 
-    def handle_request(self, request_data):
-        request_json = json.loads(request_data)
-        path = self.path
 
-        if path in endpoint_to_function:
-            self._set_response()
-            response_data = endpoint_to_function[path](request_json)
-        else:
-            self._set_response(404)
-            response_data = {'error': 'Endpoint not found.'}
+@app.route('/export_file', methods=['POST'])
+def export_file():
+    return processor.export_file(request.get_json())
 
-        response_json = json.dumps(response_data, default=str)
-        self.wfile.write(response_json.encode('utf-8'))
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        request_data = self.rfile.read(content_length).decode('utf-8')
-        self.handle_request(request_data)
+@app.route('/validate', methods=['POST'])
+def validate():
+    return processor.validate(request.get_json())
 
-    def log_message(self, format, *args):
-        # Custom logging to include timestamp
-        timestamp = time.strftime('[%Y-%m-%d %H:%M:%S]')
-        super().log_message(f'{timestamp} {format}', *args)
+
+@app.route('/kill', methods=['POST'])
+def kill_route():
+    return kill(request.get_json())
 
 
 def check_heartbeat():
@@ -102,20 +87,14 @@ def check_heartbeat():
     while True:
         if time.time() - last_heartbeat_time > heartbeat_timeout_sec:
             print('No heartbeat received. Self-destructing...')
-            # os._exit(0)
+            os._exit(0)
         time.sleep(1)
 
 
-def run_server():
+def run_server(engine):
     host = '127.0.0.1'
     port = 8080
-    server_address = (host, port)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('engine', type=str, choices=['duckdb', 'polars'])
-    args = parser.parse_args()
-
-    engine = args.engine
     init_engine(engine)
 
     heartbeat_thread = threading.Thread(target=check_heartbeat)
@@ -123,12 +102,14 @@ def run_server():
     heartbeat_thread.start()
 
     try:
-        server = ThreadingHTTPServer(server_address, RequestHandler)
+        app.run(host=host, port=port)
         print(f'Starting server on http://{host}:{port}')
-        server.serve_forever()
     except KeyboardInterrupt:
         print('Server stopped.')
 
 
 if __name__ == '__main__':
-    run_server()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('engine', type=str, choices=['duckdb', 'polars'])
+    args = parser.parse_args()
+    run_server(args.engine)
