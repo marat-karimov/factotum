2100a5bd788a3193e2ab90d0c30adb90e53bf43e
Marat Karimov <marat.karimov@osome.com>
2023-11-18 12:52:35 +0800

    Added export file test

--- server/tests/helpers/make_request.py
+++ server/tests/helpers/make_request.py
@@ -2,7 +2,12 @@ import requests
 
 host = "http://127.0.0.1:49213"
 
+
 def make_request(endpoint, payload):
     url = f"{host}{endpoint}"
-    response = requests.post(url, json=payload)
-    return response.json()
+    try:
+        response = requests.post(url, json=payload)
+        return response.json()
+    except Exception as err:
+        raise RuntimeError(
+            f"Error occurred during request to {url}: {err}") from err
