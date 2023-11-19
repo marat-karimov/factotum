217194080aeeaeabb62f9b92c46f0ffcb9f2ce78
Marat Karimov <marat.karimov@osome.com>
2023-11-19 19:41:33 +0800

    Added validate py test

---
+++ server/tests/validate_test.py
@@ -0,0 +1,37 @@
+import pytest
+from glob import glob
+
+from .helpers.make_request import make_request
+
+engines = ["polars", "duckdb"]
+
+
+@pytest.mark.parametrize("start_server", engines, indirect=True)
+class TestEngine:
+    def test_validate_endpoint_correct_sql(self, start_server, import_file):
+        """
+        Tests the validate endpoint with correct sql query.
+        """
+
+        sql = "--comment\n select * from test2; select * from test"
+
+        response = make_request("/validate", {"data": sql})
+
+        assert response.get('result')
+        assert response.get('last_statement') == "select * from test"
+        assert response.get('sql') == "select * from test2;select * from test"
+        assert response.get('error') is None
+
+    def test_validate_endpoint_wrong_sql(self, start_server, import_file):
+        """
+        Tests the validate endpoint with wrong sql query.
+        """
+
+        sql = "--comment\n select * from test2; select * from test1"
+
+        response = make_request("/validate", {"data": sql})
+
+        assert response.get('result') is False
+        assert response.get('last_statement') == "select * from test1"
+        assert response.get('sql') == "select * from test2;select * from test1"
+        assert response.get('error')
