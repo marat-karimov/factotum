52973c48f5bc81a28fc24638eef96429fb413f74
Marat Karimov <m.karimov@sweedpos.com>
2025-01-06 15:44:38 -0300

    Bug fix: handle duplicate column names when write to .por

--- server/src/pyreadstat_wrapper.py
+++ server/src/pyreadstat_wrapper.py
@@ -9,13 +9,42 @@ def strip_non_alpha(s: str):
     return s
 
 
+def deduplicate_with_suffix(strings):
+    max_length = 8
+    occurrences = {}
+    deduplicated = []
+
+    for string in strings:
+        suffix = ""
+        base_string = string
+
+        while True:
+            if suffix:
+                base_length = max_length - len(suffix)
+                truncated_string = base_string[:base_length]
+                candidate = truncated_string + suffix
+            else:
+                candidate = base_string[:max_length]
+
+            if candidate not in occurrences:
+                occurrences[candidate] = 1
+                deduplicated.append(candidate)
+                break
+            else:
+                occurrences[candidate] += 1
+                suffix = f"_{occurrences[candidate] - 1}"
+
+    return deduplicated
+
+
 def read_por(path):
     df: pd.DataFrame = pyreadstat.read_por(path)[0]
     return df
 
 
 def write_por(path: str, df: pd.DataFrame):
-    df.columns = [x[:8] for x in df.columns]  # The format has a limitation of 8 chars for column
+    df.columns = [strip_non_alpha(x) for x in df.columns]
+    df.columns = deduplicate_with_suffix(df.columns)
     pyreadstat.write_por(df, path)
 
 
