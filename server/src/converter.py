d17cbcec4b6070c4710992d988b6315c437b47ae
Marat Karimov <m.karimov@sweedpos.com>
2025-01-06 15:09:54 -0300

    Performance: use dask and parquet for temp files

--- server/src/converter.py
+++ server/src/converter.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import dask.dataframe as dd
 import tempfile
 import shutil
 import hashlib
@@ -52,24 +53,29 @@ class ReadConverter:
 
     def xpt_to_csv(self, path, **kwargs):
         return self._read_file_once(read_xpt, path, **kwargs)
-    
+
     def pkl_to_csv(self, path, **kwargs):
         return self._read_file_once(pd.read_pickle, path, **kwargs)
 
     def _read_file_chunks(self, read_func, path, chunksize, **kwargs):
         temp_file_path = self._generate_temp_file_path(path)
-        first_chunk = True
+        
+        if os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
+
+        chunk_counter = 0
 
         for file_path in glob(path):
             for chunk in read_func(file_path, chunksize=chunksize, **kwargs):
                 chunk[filename_column] = file_path
-                chunk.to_csv(temp_file_path,
-                             mode='a',
-                             header=first_chunk,
-                             index=False)
-                first_chunk = False
+                chunk.columns = chunk.columns.map(str)
+                dask_df = dd.from_pandas(chunk, npartitions=1)
+                dask_df.to_parquet(temp_file_path, name_function=lambda index: f"data-{chunk_counter}-{index}.parquet", write_index=False)
+                chunk_counter += 1
+                del chunk
+                del dask_df
 
-        return temp_file_path
+        return temp_file_path + "/*.parquet"
 
     def _read_file_once(self, read_func, path, **kwargs):
         temp_file_path = self._generate_temp_file_path(path)
@@ -77,8 +83,8 @@ class ReadConverter:
         if os.path.exists(temp_file_path):
             os.remove(temp_file_path)
 
-        first_file = True
         cols_number_expected = None
+        file_counter = 0
 
         for file_path in glob(path):
             df: pd.DataFrame = read_func(file_path, **kwargs)
@@ -94,17 +100,25 @@ class ReadConverter:
                 )
 
             df[filename_column] = file_path
-            df.to_csv(temp_file_path, mode='a', header=first_file, index=False)
-            first_file = False
+            print('aaa', temp_file_path)
+            df.columns = df.columns.map(str)
+            dask_df = dd.from_pandas(df, npartitions=1)
+            dask_df.to_parquet(temp_file_path, name_function=lambda x: f"data-{file_counter}-{x}.parquet", append=False,
+                               write_index=False)
             del df
+            del dask_df
+            file_counter += 1
 
-        return temp_file_path
+        return temp_file_path + "/*.parquet"
 
     def _generate_temp_file_path(self, path):
         hash_name = hashlib.sha256(path.encode()).hexdigest()
-        temp_file_path = os.path.join(self.temp_dir, hash_name + ".csv")
+        temp_file_path = os.path.join(self.temp_dir, hash_name)
 
         return temp_file_path
+
+    def _name_function(counter, partition_number):
+        return f"data-{counter}-{partition_number}.parquet"
 
     def cleanup(self):
         shutil.rmtree(self.temp_dir)
